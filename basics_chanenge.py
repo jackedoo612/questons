64fad6bd693d9a2321af490a57886a720f58c072
jackedoo612 <148591303+jackedoo612@users.noreply.github.com>
2024-08-31 13:03:32 -0500

    Add files via upload

---
+++ basics_chanenge.py
@@ -0,0 +1,22 @@
+name= "what is you name"
+age = "what is your age!? lad"
+school = "now what school do you go to?"
+yes = "yes"
+no = "no"
+
+print(name)
+name = input()
+print("What a cool name that is ", name)
+print("now how old are you? ",name)
+age = input()
+print("what a cool age to be!",name)
+print("now what school do you go to?")
+school = input()
+print("Thats a cool school is the name of the school?",school)
+user_input = input()
+if user_input == "yes":
+    print("cool thanks for confirming Have a good day and thats for the info") 
+else:
+    print("WHY DID YOU LIE >:( ")
+
+
